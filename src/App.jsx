ae1618172916fd8c8939809d23c40fe31449b26e
rucharajmane <rucharajmane18@gmail.com>
2024-10-10 01:41:37 +0530

    About section is done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from "./sections/Navbar.jsx";
 import Hero from "./sections/Hero.jsx";
 import About from "./sections/About.jsx";
+import Projects from "./sections/Projects.jsx";
 
 const App = () => {
     return (
@@ -9,6 +10,7 @@ const App = () => {
             <Navbar />
             <Hero />
             <About />
+            <Projects />
         </main>
     )
 }
