18c73926e77c4197c5291d0387eb24fdb5048f58
rucharajmane <rucharajmane18@gmail.com>
2024-10-10 15:45:05 +0530

    Clients and Footer sections done

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Navbar from "./sections/Navbar.jsx";
 import Hero from "./sections/Hero.jsx";
 import About from "./sections/About.jsx";
 import Projects from "./sections/Projects.jsx";
+import {Clients} from "./sections/Clients.jsx";
+import Footer from "./sections/Footer.jsx";
 
 const App = () => {
     return (
@@ -11,6 +13,8 @@ const App = () => {
             <Hero />
             <About />
             <Projects />
+            <Clients />
+            <Footer />
         </main>
     )
 }
