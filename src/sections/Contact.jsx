66936c3918d9fa47b49e7e87656285695de3ca63
rucharajmane <rucharajmane18@gmail.com>
2024-11-01 23:40:30 +0530

    Changes Done

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -74,11 +74,12 @@ const Contact = () => {
 
                 <div className="contact-container">
                     <h3 className="head-text">Let's connect</h3>
-                    <p className="text-lg text-white-600 mt-2">
+                    <p className="text-lg text-white-600 mt-3">
                         Whether you’re looking to build a new website, improve your existing platform, or bring a unique project to
-                        life, I’m here to help.</p>
+                        life, I’m here to help.
+                    </p>
 
-                    <form ref={formRef} onSubmit={handleSubmit} className="mt-4 flex flex-col space-y-7">
+                    <form ref={formRef} onSubmit={handleSubmit} className="mt-12 flex flex-col space-y-7">
                         <label className="space-y-3">
                             <span className="field-label">Full Name</span>
                             <input
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -25,12 +25,13 @@ const Hero = () => {
         <section className = "min-h-screen w-full flex flex-col relative">
             <div className="w-full mx-auto flex flex-col sm:mt-36 mt-16 c-space gap-3">
                 <p className="sm:text-4xl text-xl font-medium text-white text-center font-generalsans">
-                    Hi, I am Rucha. <span className="waving-hand"> 👋 </span>
+                    Hi, I'm Rucha. <span className="waving-hand"> 👋 </span>
                 </p>
 
                 <p className="hero_tag text-gray_gradient">
                     Full Stack Developer & Graphic Designer
                 </p>
+
 
                 <div className="w-full h-full absolute inset-0">
                     {/*<Leva />*/}
